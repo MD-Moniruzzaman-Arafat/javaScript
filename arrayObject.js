7b107ebe125d71e17bef1425ca68b02488c66c2a
md-moniruzzaman-arafat <arafatmonir091@gmail.com>
2024-01-30 13:17:08 +0600

    sum for object value

--- arrayObject.js
+++ arrayObject.js
@@ -5,3 +5,5 @@ let points = [
   { x: 1, y: 1 },
 ];
 console.log(points);
+let result = points[1].x - points[0].x;
+console.log(result);
---
+++ objectArray.js
@@ -0,0 +1,14 @@
+let data = {
+  // An object with 2 properties
+  trial1: [
+    [1, 2],
+    [3, 4],
+  ], // The value of each property is an array.
+  trial2: [
+    [2, 3],
+    [4, 5],
+  ],
+  // The elements of the arrays are arrays.
+};
+
+console.log(data);
