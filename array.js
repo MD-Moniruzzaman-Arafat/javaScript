90e2ba18ba80e8845862d88a3a6c056feecb10a7
md-moniruzzaman-arafat <arafatmonir091@gmail.com>
2024-01-30 12:42:30 +0600

    array

---
+++ array.js
@@ -0,0 +1,13 @@
+let primes = [2, 3, 5, 7];
+let empty = [];
+
+console.log(primes); // print array
+
+console.log(primes[0]); // access array value
+console.log(primes.length); // access array length
+console.log(primes[primes.length - 1]); // access array last value
+primes[4] = 110; // assign new value in primes array
+console.log(primes); // print array
+primes[4] = 555; // replace value by array index
+console.log(primes); // print array
+console.log(empty); // print array
