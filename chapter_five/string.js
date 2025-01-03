127392796ab01790393ccb16bfe0bf7bd07ad39f
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-01-03 17:56:55 +0600

    chapter_five

---
+++ chapter_five/string.js
@@ -0,0 +1,27 @@
+// string literals
+// var name = "Nicholas";
+
+
+// string constructor
+// var name2 = String("Nicholas");
+// console.log(name2)
+
+
+// string  comparison
+
+// var first = "hello";
+// var second = "hello";
+
+// console.log(first == second); // true
+// console.log(first === second); // true
+
+
+// String  Methods
+var a = "hello";
+var b = "world";
+var c = a.concat(" ", b);
+var d = c.substring(5, 11);
+var e = c.charAt(1);
+// console.log(e); 
+console.log(c.startsWith("hello")); // true
+console.log(c.endsWith("world")); // true
