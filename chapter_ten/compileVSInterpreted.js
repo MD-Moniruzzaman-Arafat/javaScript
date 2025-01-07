533c2eab233b83635bc29a16cef9600fc54fdea4
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-01-08 00:08:53 +0600

    chapter_ten

---
+++ chapter_ten/compileVSInterpreted.js
@@ -0,0 +1,24 @@
+// What is a Compiler?
+// A compiler is a program that translates the entire source code into machine code (binary code) all at once before running it.
+
+// How it works:
+// Reads the whole code at once.
+// Checks for errors in the code.
+// Converts the code into an executable file (.exe) that can run without the source code.
+
+
+// What is an Interpreter?
+// An interpreter translates and executes the code line by line instead of compiling the entire code at once.
+
+// How it works:
+// Reads one line of code.
+// Translates it into machine code.
+// Executes the translated code immediately.
+// Stops if it encounters an error, reports it, and doesn't proceed further.
+
+
+// What is JIT Compilation?
+// JIT Compilation means that JavaScript code is compiled into machine code at runtime, instead of being precompiled (like C/C++) or purely interpreted (line-by-line).
+
+// It acts as a middle ground between interpretation and compilation.
+// JIT compilers dynamically optimize code during execution, improving performance significantly.
