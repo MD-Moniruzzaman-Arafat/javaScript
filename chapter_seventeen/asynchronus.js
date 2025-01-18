e0cef812ee00a23dd0543dab1bb9796af5ce5a7c
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-01-18 13:31:58 +0600

    chapter_seventeen

---
+++ chapter_seventeen/asynchronus.js
@@ -0,0 +1,8 @@
+// aschynouse programming
+// Asynchronous programming is a design pattern which ensures the non-blocking code execution. It means that the code will not wait for the result of the function before moving to the next line of code. It is used to handle multiple requests at the same time without waiting for the response of the previous request.
+
+// promises
+// Promises are used to handle asynchronous operations in JavaScript. They are easy to manage when dealing with multiple asynchronous operations where callbacks can create callback hell leading to unmanageable code.
+
+// async/await
+// The async/await is a new way to write asynchronous code in JavaScript. It is built on top of promises and allows you to write asynchronous code that looks like synchronous code.
